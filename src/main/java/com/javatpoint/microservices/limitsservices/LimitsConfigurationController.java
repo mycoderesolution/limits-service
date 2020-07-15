f9e615e0414786e0cf2773989a09c0ffddb31c27
praveshasiwal <impraveshasiwal@gmail.com>
2020-07-15 22:15:18 +0530

    first commit

---
+++ src/main/java/com/javatpoint/microservices/limitsservices/LimitsConfigurationController.java
@@ -0,0 +1,21 @@
+package com.javatpoint.microservices.limitsservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javatpoint.microservices.limitsservices.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public LimitConfiguration retriveLimitsFromConfigurations() {
+		// getting values from the properties file
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+
+}
